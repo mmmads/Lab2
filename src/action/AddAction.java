04f54dca47de801331a1693d95e492927d138194
mmmads <731801222@qq.com>
2017-10-16 00:04:07 +0800

    Lab2 git

---
+++ src/action/AddAction.java
@@ -0,0 +1,84 @@
+package action;
+
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import dao.BookDao;
+
+
+public class AddAction extends ActionSupport{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+
+
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+	public String getCountry() {
+		return Country;
+	}
+	public void setCountry(String country) {
+		Country = country;
+	}
+	public String getPublisher() {
+		return Publisher;
+	}
+	public void setPublisher(String publisher) {
+		Publisher = publisher;
+	}
+	public String getPublishDate() {
+		return PublishDate;
+	}
+	public void setPublishDate(String publishDate) {
+		PublishDate = publishDate;
+	}
+	public double getPrice() {
+		return Price;
+	}
+	public void setPrice(double price) {
+		Price = price;
+	}
+	
+	private String ISBN;
+	private String Title;
+	private String Name;
+	private int Age;
+	private String Country;
+	private String Publisher;
+	private String PublishDate;
+	private double Price;
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		BookDao bookdao=new BookDao();
+		bookdao.AddBook(ISBN, Title, Name, Age, Country, Publisher, PublishDate, Price);
+		return "add";
+	}
+	
+
+}
